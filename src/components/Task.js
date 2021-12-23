dbfa0d5c71150f64a7d602f7596e1e58b5688d32
RhiannaJS <rhianna.latshaw@gmail.com>
2021-12-23 14:06:59 -0500

    json-server, routes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import {FaTimes} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Task = ({task, onDelete, onToggle}) => {
     return (
@@ -9,6 +10,7 @@ const Task = ({task, onDelete, onToggle}) => {
                     style={{color: 'red', cursor: 'pointer'}} 
                     onClick={() => onDelete(task.id)}/></h3>
             <p>{task.day}</p>
+            <p><Link to={`/task/${task.id}`}></Link>View Details</p>
         </div>
     )
 }
